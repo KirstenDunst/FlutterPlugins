23d1d94deb75ce27c2553502314ae82c506f47ad
曹先森 <cao_shixin@yahoo.com>
2021-01-04 17:56:03 +0800

    调整安卓为java语言

---
+++ device_info_csx/example/android/app/src/main/java/cn/brainco/device_info_csx_example/MainActivity.java
@@ -0,0 +1,6 @@
+package cn.brainco.device_info_csx_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- device_info_csx/example/android/app/src/main/kotlin/cn/brainco/device_info_csx_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package cn.brainco.device_info_csx_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
